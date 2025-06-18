6bf342c1d984377eca36ba61fb7e4769b6be8268
Promise Johnson <chiemelapromise30@gmail.com>
2025-06-18 14:26:45 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,13 @@ const router = express.Router()
 const authRouter = require('./auth')
 const userRouter = require('./users')
 // const savingRouter = require('./saving')
-// const utilRouter = require('./util')
+const utilRouter = require('./util')
 // const adminRouter = require('./admin')
 
 
 router.use('/users', userRouter)
 router.use('/auth', authRouter)
-// router.use('/savings', savingRouter)
-// router.use('/utils', utilRouter)
+router.use('/utils', utilRouter)
 // router.use('/admin', adminRouter)
 
 module.exports = router
--- routes/util.js
+++ routes/util.js
@@ -10,6 +10,60 @@ router.post(ROUTE_IMAGE_UPLOAD_MULTIPLE, auth, image_uploader.array('file'),(req
     return utilController.uploadMultipleImage(req, res)
 })
 
+/**
+ * @swagger
+ * /utils/image-upload-single:
+ *   post:
+ *     summary: Upload a single image file
+ *     tags:
+ *       - File upload
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - file
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *                 description: The image file to upload
+ *     responses:
+ *       200:
+ *         description: Image uploaded successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: Image uploaded successfully
+ *                 imageUrl:
+ *                   type: string
+ *                   example: "https://yourdomain.com/uploads/image123.jpg"
+ *       400:
+ *         description: Bad request, e.g., no file uploaded or invalid file
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: No file uploaded
+ *       500:
+ *         description: Server error
+ */
+
 router.post(ROUTE_IMAGE_UPLOAD_SINGLE, auth, image_uploader.single('file'), (req, res)=>{
     const utilController = new UtilController()
     return utilController.uploadSingleImage(req, res)
