129a4e9a536e980e1942dc1e971d3b2b74eaabd9
Promise Johnson <chiemelapromise30@gmail.com>
2026-03-13 18:19:24 +0100

    update

--- util/webHookHelpers.js
+++ util/webHookHelpers.js
@@ -73,43 +73,43 @@ async function handleChargeSuccess(data) {
         return;
       }
 
-      // const subscriptionCode = data.subscription.subscription_code;
+      const subscriptionCode = data.subscription.subscription_code;
 
-      // let subscription = await SubscriptionModel.findOne({
-      //   subscriptionCode,
-      // });
+      let subscription = await SubscriptionModel.findOne({
+        subscriptionCode,
+      });
 
-      // // 4️⃣ Create subscription if it doesn't exist
-      // if (!subscription) {
-      //   console.log("Creating new subscription");
+      // 4️⃣ Create subscription if it doesn't exist
+      if (!subscription) {
+        console.log("Creating new subscription");
 
-      //   subscription = await SubscriptionModel.create({
-      //     user: metadata.payerId || user._id,
-      //     coachId: metadata.coachId,
-      //     planId: metadata.planId,
-      //     categoryId: metadata.categoryId,
-      //     status: "active",
-      //     startDate: new Date(data.paid_at),
-      //     subscriptionCode: subscriptionCode,
-      //     paystackSubscriptionId: data.subscription.id,
-      //     lastPaymentAt: new Date(data.paid_at),
-      //     currentPeriodEnd: new Date(data.subscription.next_payment_date),
-      //     nextPaymentDate: new Date(data.subscription.next_payment_date),
-      //   });
+        subscription = await SubscriptionModel.create({
+          user: metadata.payerId || user._id,
+          coachId: metadata.coachId,
+          planId: metadata.planId,
+          categoryId: metadata.categoryId,
+          status: "active",
+          startDate: new Date(data.paid_at),
+          subscriptionCode: subscriptionCode,
+          paystackSubscriptionId: data.subscription.id,
+          lastPaymentAt: new Date(data.paid_at),
+          currentPeriodEnd: new Date(data.subscription.next_payment_date),
+          nextPaymentDate: new Date(data.subscription.next_payment_date),
+        });
 
-      //   return;
-      // }
+        return;
+      }
 
-      // subscription.status = "active";
-      // subscription.lastPaymentAt = new Date(data.paid_at);
-      // subscription.currentPeriodEnd = new Date(
-      //   data.subscription.next_payment_date
-      // );
-      // subscription.nextPaymentDate = new Date(
-      //   data.subscription.next_payment_date
-      // );
+      subscription.status = "active";
+      subscription.lastPaymentAt = new Date(data.paid_at);
+      subscription.currentPeriodEnd = new Date(
+        data.subscription.next_payment_date
+      );
+      subscription.nextPaymentDate = new Date(
+        data.subscription.next_payment_date
+      );
 
-      // await subscription.save();
+      await subscription.save();
     }
   } catch (error) {
     console.error("Error in handleChargeSuccess:", error);
@@ -187,6 +187,8 @@ async function handleSubscriptionCreate(data) {
         paystackSubscriptionId: data.plan.plan_code,
         paystackAuthorizationToken: data.authorization.authorization_code,
         user: user._id,
+        currentPeriodEnd: new Date(data.next_payment_date),
+
       })
     }
   } catch (error) {
